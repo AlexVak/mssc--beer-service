eea3c50bd5b8d0ed375b18526be604bc83c78129
Oleksandr Vakarchuk <Oleksandr_Vakarchuk@epam.com>
2019-10-08 13:30:01 +0300

    add beer repository

---
+++ src/main/java/com/alexvak/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.alexvak.msscbeerservice.repositories;
+
+import com.alexvak.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
